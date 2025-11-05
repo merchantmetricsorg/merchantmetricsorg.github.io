91564b4185c168f17ed40b1572e73c72949033ed
anto457 <anto.hosek@gmail.com>
2025-11-05 15:50:03 +0100

    update url

--- svelte.config.js
+++ svelte.config.js
@@ -10,7 +10,7 @@ export default {
       strict: true
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/merchantmetrics' : ''
+      // base: process.env.NODE_ENV === 'production' ? '/merchantmetrics' : ''
     }
   }
 };
